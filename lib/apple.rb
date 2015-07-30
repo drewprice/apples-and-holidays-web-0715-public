9722ec8c5c24fa74b766c95023315fa930f65270
Drew Price <drewprice@users.noreply.github.com>
2015-07-30 19:07:43 -0400

    Refactor ternary operator

--- lib/apple.rb
+++ lib/apple.rb
@@ -1,14 +1,9 @@
 require 'pry'
 
 def apple_picker_with_select(fruit_list)
-  # fruit_list will look something like: ["apple", "banana", "apple"]
-  # you want to return an array that just has apples in it
   fruit_list.select { |e| e == 'apple' }
 end
 
 def apple_picker_with_collect(fruit_list)
-  # fruit_list will look something like: ["apple", "banana", "apple"]
-  # you want to return an array that just the apples, just like the
-  # challenge above
-  fruit_list.collect { |e| e == 'apple' ? e : nil  }.compact
+  fruit_list.collect { |e| e if e == 'apple' }.compact
 end
